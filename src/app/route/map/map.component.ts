5b583dbca18631595661b6b83603c3aea529e09e
AJ Srivastava <aj.srivastava@monsanto.com>
2016-10-22 17:45:07 -0500

    add map

--- src/app/route/map/map.component.ts
+++ src/app/route/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
     selector: 'map',
@@ -6,8 +6,17 @@ import { Component } from '@angular/core'
     providers: []
 })
 
-export class MapComponent {
+export class MapComponent extends OnInit {
     constructor() {
-        console.log(L)
+    }
+
+    ngOnInit() {
+        // create a map in the "map" div, set the view to a given place and zoom
+        var map = L.map('mapContainer', {drawControl: true}).setView([51.505, -0.09], 13);
+
+// add an OpenStreetMap tile layer
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
     }
 }

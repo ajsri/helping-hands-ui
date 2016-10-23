d29b8a9cff81de5a2047e0eb5a63c6915e6a6a0e
AJ Srivastava <aj.srivastava@monsanto.com>
2016-10-23 07:06:31 -0500

    clean up disability info

--- src/app/route/user/disabilities/disabilities.component.ts
+++ src/app/route/user/disabilities/disabilities.component.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core'
 
 export class DisabilitiesComponent {
     @Input() data: any[];
+    @Input() client: any;
     noHistory: boolean;
     tableHidden: boolean = true;
     disabilityTypes = {

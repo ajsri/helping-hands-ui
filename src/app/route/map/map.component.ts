a224ae000923cd5fd0ee96b8d9f09ea326a5d407
AJ Srivastava <aj.srivastava@monsanto.com>
2016-10-22 19:39:54 -0500

    popup markers

--- src/app/route/map/map.component.ts
+++ src/app/route/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core'
+var services = require('../../mockdata/services.json')
+
 
 @Component({
     selector: 'map',
@@ -8,6 +10,7 @@ import { Component, OnInit } from '@angular/core'
 
 export class MapComponent extends OnInit {
 
+    services = services;
     map: any;
     points: any[];
     vm: any;
@@ -17,7 +20,7 @@ export class MapComponent extends OnInit {
         var vm = this;
 
         this.map = L.map(m, {
-            center: L.latLng(38.636, -90.248),
+            center: L.latLng(38.6439633,-90.2565238),
             zoom: 16
         });
 
@@ -26,11 +29,30 @@ export class MapComponent extends OnInit {
             maxZoom: 18,
         });
 
-        L.marker([38.636, -90.248]).addTo(this.map);
+        this.services.map((service: any, i: number) => {
+            var lat = (service.lat);
+            var lon = (service.lon);
+            var content = `<div class="popup-content-svc">
+                                <p><strong>${service.name}</strong></p>
+                                <p>${service.address}</p>
+                                <p>${service.phone}</p>
+                                <p><strong>Services</strong></p>
+                                <p>${service.clothing == 1 ? 'Clothing' : ''}</p>
+                                <p>${service.community_services == 1 ? 'Community Services' : ''}</p>
+                                <p>${service.counseling == 1 ? 'Counseling' : ''}</p>
+                                <p>${service.education == 1 ? 'Education' : ''}</p>
+                                <p>${service.emergency_shelter == 1 ? 'Emergency Shelter' : ''}</p>
+                                <p>${service.employment_assitance == 1 ? 'Employment Assistance' : ''}</p>
+                                <p>${service.food == 1 ? 'Food' : ''}</p>
+                                <p>${service.healthcare == 1 ? 'Healthcare' : ''}</p>
+                                <p>${service.housing_assistance == 1 ? 'Housing Assistance' : ''}</p>
+                                <p>${service.hygience_essentials == 1 ? 'Hygiene Essentials' : ''}</p>
+                                <p>${service.legal == 1 ? 'Legal Assistance' : ''}</p>
+                                <p>${service.transportation == 1 ? 'Transportation' : ''}</p>
+                                <p>${service.utility_assistance == 1 ? 'Utility Assistance' : ''}</p>
+                           </div>`
+            L.marker([lat, lon]).bindPopup(content).openPopup().addTo(this.map);
+        })
         tileLayer.addTo(this.map);
-
-        this.map.on('click', (e: any) => {
-            L.marker([e.latlng.lat, e.latlng.lng]).addTo(this.map);
-        });
     }
 }
